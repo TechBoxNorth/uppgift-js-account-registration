2f959489a7d43150e34739e31c7a2e0706148bb2
TechBoxNorth <techboxnorth@gmail.com>
2024-01-25 17:11:12 +0100

    registration form finished, looks awesome-ish

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
+// check for matching passwords
+let pwdLengthOk = false;
+let pwdMatchOk = false;
+
+function pwdMatchCheck(){
+    if(pwdLengthOk && pwdMatchOk){
+        submitBtn.disabled = false;
+        //submitBtn.style.color = 'white';
+        //submitBtn.innerText = 'SUBMIT';
+    } else {
+        submitBtn.disabled = true;
+        //submitBtn.style.color = 'red';
+        //submitBtn.innerText = 'FORM NOT READY!';
+    }
+}
+
+// form
 const form = document.querySelector('.reg-form');
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log(nameInput.value);
+    console.log('bullarna e slut!');
+    const registrationData = {
+        name: nameInput.value,
+        username: userInput.value,
+        email: emailInput.value,
+        password: pwdInput.value
+    }
+
+    console.log(registrationData.name);
+    alert(`Name: ${registrationData.name}\nUsername: ${registrationData.username}\nEmail: ${registrationData.email}\nPassword: ${registrationData.password}`);
+    
 });
+
+
 
 // Name
 const nameLabel = document.querySelector('.name-label');
@@ -44,10 +73,22 @@ pwdLabel.addEventListener('click', () => {
 });
 const pwdInput = document.querySelector('.pwd');
 pwdInput.addEventListener('input', () => {
+    const fieldSet = document.querySelector('.pwd-field');
     const value = pwdInput.value;
     if(value.length < 8){
-        console.log('not long enough')
+        errorInfo.style.color = 'red';
+        errorInfo.innerText = 'Your password is too short!';
+        fieldSet.style.border = '1px solid red';
+        fieldSet.style.color = 'red';
+        pwdLengthOk = false;
+    } else {
+        errorInfo.style.color = 'green';
+        errorInfo.innerText = 'Password length is OK!';
+        fieldSet.style.border = '1px solid #577f97'
+        fieldSet.style.color = 'black';
+        pwdLengthOk = true;
     }
+    pwdMatchCheck();
     console.log(value);
 });
 
@@ -58,18 +99,37 @@ confPwdLabel.addEventListener('click', () => {
 });
 const confPwdInput = document.querySelector('.conf-pwd');
 confPwdInput.addEventListener('input', () => {
+    const fieldSet = document.querySelector('.conf-pwd-field');
     const value = confPwdInput.value;
     if(value != pwdInput.value){
-        console.log('does not match');
+        errorInfo.style.color = 'red';
+        errorInfo.innerText = `Your password doesn't match!`;
+        fieldSet.style.border = '1px solid red';
+        fieldSet.style.color = 'red';
+        pwdMatchOk = false;
+    } else {
+        errorInfo.style.color = 'green';
+        errorInfo.innerText = 'Password is a match!';
+        fieldSet.style.border = '1px solid #577f97'
+        fieldSet.style.color = 'black';
+        pwdMatchOk = true;
     }
+    pwdMatchCheck();
     console.log(value);
 });
 
+const errorInfo = document.querySelector('.error-info');
+errorInfo.innerText = '';
+
+
 // button
 const submitBtn = document.querySelector('button');
+/*
 submitBtn.addEventListener("click", (e) => {
-    //console.log('This is the event: ', e.target);
+    console.log('This is the event: ', e.target);
 })
+*/
+
 
 
 
