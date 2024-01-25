80b4f7c4f4d2a699bc9b48f49c0a9b8967a98a3b
TechBoxNorth <techboxnorth@gmail.com>
2024-01-25 12:03:09 +0100

    changed back to labels having eventlisteners

--- index.js
+++ index.js
@@ -4,24 +4,44 @@ form.addEventListener("submit", (e) => {
     console.log('Form was submitted!');
 });
 
+// Name
+const nameLabel = document.querySelector('.name-label');
+nameLabel.addEventListener('click', () => {
+    nameInput.focus();
+});
 const nameInput = document.querySelector('.name');
 nameInput.addEventListener('input', () => {
     const value = nameInput.value;
     console.log(value);
 });
 
+// User name
+const userLabel = document.querySelector('.user-label');
+userLabel.addEventListener('click', () => {
+    userInput.focus();
+});
 const userInput = document.querySelector('.user');
 userInput.addEventListener('input', () => {
     const value = userInput.value;
     console.log(value);
 });
 
+// email
+const emailLabel = document.querySelector('.e-mail-label');
+emailLabel.addEventListener('click', () => {
+    emailInput.focus();
+});
 const emailInput = document.querySelector('.e-mail');
 emailInput.addEventListener('input', () => {
     const value = emailInput.value;
     console.log(value);
 });
 
+// password
+const pwdLabel = document.querySelector('.pwd-label');
+pwdLabel.addEventListener('click', () => {
+    pwdInput.focus();
+});
 const pwdInput = document.querySelector('.pwd');
 pwdInput.addEventListener('input', () => {
     const value = pwdInput.value;
@@ -31,6 +51,11 @@ pwdInput.addEventListener('input', () => {
     console.log(value);
 });
 
+// confirm password
+const confPwdLabel = document.querySelector('.conf-pwd-label');
+confPwdLabel.addEventListener('click', () => {
+    confPwdInput.focus();
+});
 const confPwdInput = document.querySelector('.conf-pwd');
 confPwdInput.addEventListener('input', () => {
     const value = confPwdInput.value;
@@ -40,6 +65,7 @@ confPwdInput.addEventListener('input', () => {
     console.log(value);
 });
 
+// button
 const submitBtn = document.querySelector('button');
 submitBtn.addEventListener("click", (e) => {
     console.log('This is the event: ', e.target);
@@ -50,46 +76,3 @@ submitBtn.addEventListener("click", (e) => {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-const nameLabel = document.querySelector('.name-label');
-const nameInput = document.querySelector('.name');
-nameLabel.addEventListener('click', () => {
-    nameInput.focus();
-});
-
-const userLabel = document.querySelector('.user-label');
-const userInput = document.querySelector('.user');
-userLabel.addEventListener('click', () => {
-    userInput.focus();
-});
-
-const emailLabel = document.querySelector('.e-mail-label');
-const emailInput = document.querySelector('.e-mail');
-emailLabel.addEventListener('click', () => {
-    emailInput.focus();
-});
-
-const pwdLabel = document.querySelector('.pwd-label');
-const pwdInput = document.querySelector('.pwd');
-pwdLabel.addEventListener('click', () => {
-    pwdInput.focus();
-});
-
-const confPwdLabel = document.querySelector('.conf-pwd-label');
-const confPwdInput = document.querySelector('.conf-pwd');
-confPwdLabel.addEventListener('click', () => {
-    confPwdInput.focus();
-});
-*/
